3caad6cced7730ec96db60b252b0845ba96a3663
Shimao890 <86386251+Shimao890@users.noreply.github.com>
2021-06-24 16:30:42 -0700

    config

---
+++ rocketpatrol/src/scene/play.js
@@ -0,0 +1,16 @@
+const { Phaser } = require("../../lib/phaser");
+
+class play extends Phaser.scene {
+    constructor() {
+        super("playScene");
+
+    }
+preload() {
+    this.load.image();
+}
+create() {
+    this.add.text(20,20, "Rocekt Patrol Play");
+    //green UI background
+
+}
+}
