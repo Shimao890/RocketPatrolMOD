63a430441b8edfafdc8a84cbedce2fc1a4cce08d
Shimao890 <86386251+Shimao890@users.noreply.github.com>
2021-06-29 00:32:59 -0700

    add new ship

--- rocketpatrolMOD/src/prefab/Spaceship.js
+++ rocketpatrolMOD/src/prefab/Spaceship.js
@@ -7,7 +7,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
     }
     update() {
         this.x -= this.moveSpeed;
-        if(this.x <= 0-this.width){
+        if(this.x <= 0 - this.width){
             this.reset();
         }
     }
@@ -15,3 +15,4 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x = game.config.width;
     }
 }
+
